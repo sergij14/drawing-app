82623ad2721a4d83b97c8a20fe95c1a68bb507dc
sergij14 <sergi.jajanidze@gmail.com>
2022-11-23 18:12:12 +0400

    created canvas element & handling mousemove / migrated to parcel bundler

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import { fromEvent } from "rxjs";
+import { map } from "rxjs/operators";
+
+const canvas = document.querySelector("canvas");
+const range = document.getElementById("range");
+const color = document.getElementById("color");
+
+const ctx = canvas.getContext("2d");
+const rect = canvas.getBoundingClientRect();
+const scale = window.devicePixelRatio;
+
+canvas.width = rect.width * scale;
+canvas.height = rect.height * scale;
+ctx.scale(scale, scale);
+
+const mouseMove$ = fromEvent(canvas, "mousemove");
+const mouseDown$ = fromEvent(canvas, "mousedown");
+const mouseUp$ = fromEvent(canvas, "mouseup");
+const mouseOut$ = fromEvent(canvas, "mouseout");
+
+mouseMove$
+  .pipe(
+    map((e) => ({
+      x: e.offsetX,
+      y: e.offsetY,
+    }))
+  )
+  .subscribe((pos) => {
+    console.log(pos);
+    ctx.fillRect(pos.x, pos.y, 2, 2);
+  });
